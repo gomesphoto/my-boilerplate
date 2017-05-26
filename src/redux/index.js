82e2c98e28afe691117785d2dc091aadcb74e297
Pedro Gomes <pedro@loot.io>
2017-05-26 19:25:24 +0100

    update new create-react-app

--- src/redux/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { authenticationReducer } from './_authentication';
-
-export default combineReducers({
-  authentication: authenticationReducer
-});
