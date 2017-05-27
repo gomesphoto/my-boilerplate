84f7ab015717e7c7cddd3ca89cc7b2d55bf7ba07
Pedro Gomes <pedro@loot.io>
2017-05-27 11:35:50 +0100

    add src base

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { injectGlobal } from 'styled-components';
+import { globalStyles } from './styles';
+import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line
+injectGlobal`${globalStyles}`;
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
 registerServiceWorker();
